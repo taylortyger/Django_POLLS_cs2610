539170c0b71ded9f333ef3c2cd3c47ccd3b31d84
Taylor Tyger <taylortyger@gmail.com>
2017-04-14 23:48:02 +0000

    Changed index view to use shortcut render() function instead of the long way of loading a template.

--- polls/views.py
+++ polls/views.py
@@ -1,16 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from django.template import loader
 from .models import Question
 
 # Create your views here.
 def index(request):
     question_list = Question.objects.order_by('-pub_date')
-    template = loader.get_template('polls/index.html')
     context = {
         'question_list': question_list,
     }
-    return HttpResponse(template.render(context, request))
+    return render(request, 'polls/index.html', context)
     
 def detail(request, question_id):
     return HttpResponse("Well, you're looking at the details of question %s." % question_id)
