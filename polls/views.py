26ce188510775dd61d6dd0e2f37c699584a0773a
Taylor Tyger <taylortyger@gmail.com>
2017-04-14 19:17:48 +0000

    created views for question detail, question vote, and question results.

--- polls/views.py
+++ polls/views.py
@@ -3,4 +3,14 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello from the polls app")
+    return HttpResponse("hi")
+    
+def detail(request, question_id):
+    return HttpResponse("Well, you're looking at the details of question %s." % question_id)
+    
+def vote(request, question_id):
+    return HttpResponse("You are voting on question: %s" % question_id)
+    
+def results(request, question_id):
+    return HttpResponse("Here lies the results of question %s." % question_id)
+    
