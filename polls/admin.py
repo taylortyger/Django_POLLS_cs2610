f1d3baa832f84812c86dbd919b1525e4a7173c86
Taylor Tyger <taylortyger@gmail.com>
2017-04-16 20:57:29 +0000

    Made the admin area better by adding an area to modify the choices of a question on the page for modifying a question.

--- polls/admin.py
+++ polls/admin.py
@@ -1,6 +1,20 @@
 from django.contrib import admin
 from . import models
 
-# Register your models here.
-admin.site.register(models.Question)
+
+class ChoiceInline(admin.TabularInline):
+    model = models.Choice
+    extra = 3
+
+
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None,               {'fields': ['question_text']}),
+        ('Date information', {'fields': ['pub_date'], 'classes': ['collapse']}),
+    ]
+    inlines = [ChoiceInline]
+    list_display = ('question_text', 'pub_date')
+    
+
+admin.site.register(models.Question, QuestionAdmin)
 admin.site.register(models.Choice)
